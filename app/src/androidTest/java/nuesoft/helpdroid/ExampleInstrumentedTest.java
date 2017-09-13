c2bd50df76f9445b17895178dcfc99cf3e38c987
Mysterious <ehsan.mashhadi@gmail.com>
2017-09-13 19:41:33 +0430

    Add exclude, init jacco reporter

--- app/src/androidTest/java/nuesoft/helpdroid/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/nuesoft/helpdroid/ExampleInstrumentedTest.java
@@ -20,7 +20,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-
-        assertEquals("nuesoft.droidhelper", appContext.getPackageName());
+        assertEquals("nuesoft.helpdroid.application", appContext.getPackageName());
     }
 }
