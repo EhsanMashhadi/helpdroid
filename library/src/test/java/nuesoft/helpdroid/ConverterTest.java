555db24e956625729ea30bf97d7ae97e14c72138
miladsalimi <miladsalimi69@yahoo.com>
2018-05-04 21:44:50 +0430

    Create byteArrayToStringTest and byteArrayToUnicodeStringTest in converterTest

--- library/src/test/java/nuesoft/helpdroid/ConverterTest.java
+++ library/src/test/java/nuesoft/helpdroid/ConverterTest.java
@@ -1,10 +1,12 @@
 package nuesoft.helpdroid;
 
-import junit.framework.Assert;
+
 
 import org.junit.Test;
 
 import nuesoft.helpdroid.util.Converter;
+
+import org.junit.Assert;
 
 /**
  * Created by mysterious on 9/16/17.
@@ -53,4 +55,26 @@ public class ConverterTest {
         String hexString = Converter.bytesToHexString(bytes);
         Assert.assertEquals(hexString, "A1B2C3");
     }
+
+    @Test
+    public void byteArrayToStringTest(){
+
+        byte[] bytes=new byte[2];
+        bytes[0]=65;
+        bytes[1]=66;
+        Assert.assertEquals("AB",Converter.byteArrayToString(bytes));
+        Assert.assertNotEquals("",Converter.byteArrayToString(bytes));
+        Assert.assertNotEquals(null,Converter.byteArrayToString(bytes));
+    }
+
+    @Test
+    public void byteArrayToUnicodeString(){
+
+        byte[] bytes=new byte[2];
+        bytes[0]=65;
+        bytes[1]=66;
+        Assert.assertEquals("AB",Converter.byteArrayToUnicodeString(bytes));
+
+    }
+
 }
