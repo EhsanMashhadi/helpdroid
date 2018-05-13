bad7003fa57062bc52d223375dc2da4df5e32a80
Mysterious <ehsan.mashhadi@gmail.com>
2018-05-13 16:30:20 +0430

    Remove extra code

--- app/src/androidTest/java/nuesoft/helpdroid/SecurityTest.java
+++ app/src/androidTest/java/nuesoft/helpdroid/SecurityTest.java
@@ -2,7 +2,6 @@ package nuesoft.helpdroid;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.provider.Settings;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -20,26 +19,26 @@ import nuesoft.helpdroid.device.Security;
 public class SecurityTest {
 
     @Test
-    public void isDeveloperOptionOn() throws Settings.SettingNotFoundException {
+    public void isDeveloperOptionOn() {
 
         Context context = InstrumentationRegistry.getTargetContext();
-        boolean isDeveloperOptionOn= Security.isDeveloperOptionOn(context);
+        boolean isDeveloperOptionOn = Security.isDeveloperOptionOn(context);
         Assert.assertTrue(isDeveloperOptionOn);
     }
 
     @Test
     public void isPackageNameTampered() {
 
-        Context context=InstrumentationRegistry.getTargetContext();
-        boolean isPackageNameTampered=Security.isPackageNameTampered(context,"nuesoft.helpdroid.application");
+        Context context = InstrumentationRegistry.getTargetContext();
+        boolean isPackageNameTampered = Security.isPackageNameTampered(context, "nuesoft.helpdroid.application");
         Assert.assertTrue(!isPackageNameTampered);
     }
 
     @Test
     public void isCertificateTampered() throws CertificateException, NoSuchAlgorithmException, PackageManager.NameNotFoundException {
 
-        Context context=InstrumentationRegistry.getTargetContext();
-        boolean isCertificateTampered =Security.isCertificateTampered(context,"E31DE24C0976371BCAE8810C25F4D13473DE4FE0",
+        Context context = InstrumentationRegistry.getTargetContext();
+        boolean isCertificateTampered = Security.isCertificateTampered(context, "E31DE24C0976371BCAE8810C25F4D13473DE4FE0",
                 "87DBE3FCBDFEACE63FF1E59ED73CDB04B670E0A9B517A6CE96EE5A0117FD6737");
         Assert.assertFalse(isCertificateTampered);
     }
@@ -47,16 +46,16 @@ public class SecurityTest {
     @Test
     public void isRooted() {
 
-        Context context=InstrumentationRegistry.getTargetContext();
-        boolean isRooted=Security.isRooted(context);
+        Context context = InstrumentationRegistry.getTargetContext();
+        boolean isRooted = Security.isRooted(context);
         Assert.assertFalse(isRooted);
     }
 
     @Test
     public void isRootedWithoutBusyBox() {
 
-        Context context=InstrumentationRegistry.getTargetContext();
-        boolean isRootedWithoutBusyBox=Security.isRootedWithoutBusyBox(context);
+        Context context = InstrumentationRegistry.getTargetContext();
+        boolean isRootedWithoutBusyBox = Security.isRootedWithoutBusyBox(context);
         Assert.assertFalse(isRootedWithoutBusyBox);
     }
 }
